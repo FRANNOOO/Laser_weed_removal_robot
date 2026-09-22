3a2683157a37c947274dfd465f996ef7be8c3051
Daniel Suhr <dsuhr@atb-potsdam.de>
2026-09-22 11:47:55 +0200

    push

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,3 @@
 rsync -avh --delete docker-compose.yaml fieldfriend_f16:projects/app_weed_removal_state_machine/
 rsync -avh --delete src fieldfriend_f16:projects/app_weed_removal_state_machine/src
+rsync -avh --delete config_zenoh_session.json5 fieldfriend_f16:projects/app_weed_removal_state_machine/config_zenoh_session.json5
